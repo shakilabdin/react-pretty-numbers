1ba7eab59baf51658b96ed634730a05298f234af
shakilabdin <56568494+shakilabdin@users.noreply.github.com>
2021-01-20 18:52:47 -0500

    Update numeric_label.js
    Changed 1E9 from G to B

--- src/component/numeric_label.js
+++ src/component/numeric_label.js
@@ -17,7 +17,7 @@ const NumericLabel = (props) => {
       { value: 1E18, symbol: "E" },
       { value: 1E15, symbol: "P" },
       { value: 1E12, symbol: "T" },
-      { value: 1E9,  symbol: "G" },
+      { value: 1E9,  symbol: "B" },
       { value: 1E6,  symbol: "M" },
       { value: 1E3,  symbol: "k" }
     ], rx = /\.0+$|(\.[0-9]*[1-9])0+$/, i;
